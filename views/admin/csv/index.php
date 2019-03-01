10c74cec903e29bcbc5d9f63dccf496ebf8b6ffb
Franck Dupont <kyfr59@gmail.com>
2019-03-01 14:44:29 +0100

    Ajout des instructions pour l'import et du fichier modèle

--- views/admin/csv/index.php
+++ views/admin/csv/index.php
@@ -13,26 +13,19 @@ echo head(
 <p>Procédure d'import en CSV : </p>
 
 <ul>
-    <li>Installez le <a target="_blank" href="https://www.zotero.org/download/">logiciel Zotero 5.0</a> pour votre système d'exploitation</li>
-    <li>Configurez le logiciel pour gérer votre bibliothèque</li>
-    <li>Cliquez droit sur votre bibliothèque et choisissez <strong>"Exporter la bibliothèque"</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/exporter.png'; ?>">copie d'écran</a></li>
-    <li>Enregistrez le fichier au format CSV avec l'encodage <strong>"Unicode (UTF-8 sans BOM)"</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/enregistrer.png'; ?>">copie d'écran</a></li>
+    <li>Téléchargez le <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB ?>modele.xlsx">fichier modèle</a> sur votre ordinateur</li>
+    <li>Ouvrez-le avec Microsoft Excel</li>
+    <li>Remplissez-le en respectant les entêtes de colonnes</li>
+    <li>La première ligne est à supprimer, elle est donnée à titre d'exemple, mais la ligne contenant les entêtes doit être conservée</li>
+    <li>Une fois le fichier dûment rempli, choisissez <strong>"Enregister sous"</strong>, puis <strong>"Autres formats"</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/autres-formats.png'; ?>">copie d'écran</a></li>
+    <li>Dans la boite de dialogue d'enregistrement, choisissez <strong>CSV (séparateur virgule)</strong> dans la zone <strong>Type</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/enregistrer-au-format-csv.png'; ?>">copie d'écran</a></li>
     <li>Cliquez sur le bouton "Choisissez un fichier" ci-dessous</li>
     <li>Sélectionnez votre fichier CSV</li>
+    <li>Sélectionnez la collection dans laquelle importer les données (celle-ci doit être définie comme publique dans Omeka)</li>
     <li>Cliquez sur le bouton "Vérifier les données" ci-dessous</li>
-    <li>Une page vous présente une prévisualisation des données de votre fichier</li>
-    <li><b style="color:orangered;">Vérifiez que la prévisualisation ne présente pas de problèmes d'accentuation</b>, si c'est le cas vérifiez que le fichier CSV est bien <b>encodé en UTF-8</b></li>
+    <li>Une page vous présente une prévisualisation des données de votre fichier (les données récupérées depuis la BnF sont présentées dans un sous-tableau)</li>
+    <li><b style="color:orangered;">Vérifiez que la prévisualisation ne présente pas de problèmes d'accentuation ou d'apostrophes</b>, si c'est le cas vérifiez que le fichier CSV est bien <b>encodé en UTF-8</b></li>
 </ul>
-
-<?php
-    echo "<h1 style='margin-top:30px; margin-bottom:-10px;'>Date du dernier import : ";
-    $date = BibliothequeArtistesImport::getLastImportDate();
-    if ($date)
-        echo $date;
-    else
-        echo "aucun import réalisé";
-    echo "</h1>";
-?>
 
 <br /><br />
 <form action="#" method="post"  enctype="multipart/form-data" accept-charset="utf-8">
