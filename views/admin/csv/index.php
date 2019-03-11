eb2ea465169de06c059f8bd05af93570a3d36aca
Franck Dupont <kyfr59@gmail.com>
2019-03-11 16:59:30 +0100

    Finalisation des textes sur le processus d'import

--- views/admin/csv/index.php
+++ views/admin/csv/index.php
@@ -22,14 +22,19 @@ echo head(
     <li>Vous pouvez utiliser le séparateur <strong>#</strong> pour ajouter plusieurs valeurs dans le même champ, par exemple pour <strong>Français#Anglais</strong> pour ajouter 2 langues</li>
     <li>Il est conseiller de ne pas importer plus de 50 lignes (notices) lors du même import</li>
     <li>Une fois le fichier dûment rempli, choisissez <strong>"Enregister sous"</strong>, puis <strong>"Autres formats"</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/autres-formats.png'; ?>">copie d'écran</a></li>
-    <li>Dans la boite de dialogue d'enregistrement, choisissez <strong>CSV (séparateur virgule)</strong> dans la zone <strong>Type</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/enregistrer-au-format-csv.png'; ?>">copie d'écran</a></li>
-    <li>Cliquez sur le bouton "Choisissez un fichier" ci-dessous</li>
+    <li>Dans la boite de dialogue d'enregistrement, choisissez <strong>CSV (séparateur point-virgule)</strong> dans la zone <strong>Type</strong> - voir <a target="_blank" href="<?php echo BIBLIOTHEQUEARTISTES_PLUGIN_WEB . '/images/enregistrer-au-format-csv.png'; ?>">copie d'écran</a></li>
+    <li>Sélectionnez, ci-dessous, la collection dans laquelle importer les données (celle-ci doit être définie comme publique dans Omeka)</li>
+    <li>Cliquez sur le bouton "Parcourir" ci-dessous</li>
     <li>Sélectionnez votre fichier CSV</li>
-    <li>Sélectionnez la collection dans laquelle importer les données (celle-ci doit être définie comme publique dans Omeka)</li>
     <li>Cliquez sur le bouton "Vérifier les données" ci-dessous</li>
     <li>Une page vous présente une prévisualisation des données de votre fichier (les données récupérées depuis la BnF sont présentées dans un sous-tableau)</li>
-    <li><b style="color:orangered;">Vérifiez que la prévisualisation ne présente pas de problèmes d'accentuation ou d'apostrophes</b>, si c'est le cas vérifiez que le fichier CSV est bien <b>encodé en UTF-8</b></li>
+    <li><b style="color:orangered;">Vérifiez que la prévisualisation ne présente pas de problèmes d'accentuation ou d'apostrophes</b>, si c'est le cas vérifiez que le fichier CSV est bien encodé en UTF-8</li>
+    <li>Sur cette fenêtre cliquer sur" Lancer l'import des notices".</li>
+    <li>Si tout s'est bien passé, vous revenez à l'écran précédent avec une mention du nombre de lignes importées. Vérifiez qu'il y en a le même nombre que dans votre fichier.</li>
 </ul>
+
+
+
 
 <br /><br />
 <form action="#" method="post"  enctype="multipart/form-data" accept-charset="utf-8">
